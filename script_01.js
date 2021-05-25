bda2c9062efd579ff5f8f63ef4a9e30713d6b529
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-05-25 12:42:32 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,16 @@
+
+/*** Mini-Challenge  */
+// Satzbau + Punktuation
+
+// Vereinbarung:
+// "S" (sentence) --> "."
+// "Q" (question) --> "?"
+// "E" (exclamation) --> "!"
+
+
+/*** Funktion mit Array als Parameter */
+// mögliche Tests:
+// ausgabe(getSentence(["Ich","bin","Peter"],"S"));
+// ausgabe(getSentence(["Bist","du","Peter"],"Q"));
+// ausgabe(getSentence(["Ich","bin"],"E"));
+// ausgabe(getSentence(["Ich","bin"],"#"));
